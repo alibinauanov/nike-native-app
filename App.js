edb5192c7af6e09331affcdc3381a95ab6019dc7
alibinauanov <an3502@nyu.edu>
2023-06-19 17:06:30 +0600

    Product Details Screen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import products from './src/data/products';
 import ProductsScreen from './src/screens/ProductsScreen';
+import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
+// import products from './src/data/products';
 // import Navigation from './src/navigation';
 // import { Provider } from 'react-redux';
 // import { store } from './src/store';
@@ -13,7 +14,8 @@ import ProductsScreen from './src/screens/ProductsScreen';
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductsScreen />
+      {/* <ProductsScreen /> */}
+      <ProductDetailsScreen />
 
       <StatusBar style="auto" />
     </View>
@@ -23,8 +25,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#fff'
   }
 });
